360ee157bb30474cea9bbcccfced329a3539fb55
Adam Clayman <adam@clayman.org>
2017-09-30 01:59:54 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,8 +3,8 @@ let key = 0;
 
 function init() {
   // write your code within this function declaration
-  document.addEventListener('keydown', function(e) {
-    if (e.which === code[key]) {
+  document.addEventListener('keydown', function(event) {
+    if (event.which === code[key]) {
 
       if (key === 9) {
         window.alert('Congrats! You found the Konami code!');
