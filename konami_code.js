59229db6fcb80f8a29deefdbee9d516156ae767f
Adam Clayman <adam@clayman.org>
2017-09-30 01:57:37 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,48 +2,17 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let key = 0;
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
-  for (let i = 0; i < code.length; i++) {
-    document.querySelector('input').addEventListener('keydown', function charListener(e) {
-      if(code[i] === e.which && key === i) {
-        if (key === 9) {
-          alert('Congrats! You found the Konami code!');
-        }
-        key = i++;
-      }
-    });
-  }
-}
-
-/*
-  document.body.addEventListener('keydown', function(event) {
-    // Increment key if the keycode matches.
-    console.log(event.which);
-    if (code[key] === event.which) {
-
+  // write your code within this function declaration
+  document.addEventListener('keydown', function(e) {
+    if (e.which === code[key]) {
+      key++;
       if (key === 9) {
         alert('Congrats! You found the Konami code!');
       }
-      key++;
     }
     else {
       key = 0;
     }
   });
 }
-*/
-/*
-  document.body.addEventListener('keydown', function(event) {
-    if (event.which === '38') {
-      for (i = 1; i < code.length; i++) {
-        if (Object.keys(event)[i] === code[i]) {
-          continue;
-        }
-        else return null;
-      }
-    }
-    alert('Congrats! You found the Konami code!');
-  });
-}
-*/
+
