6377e5b17c10ae143864d853b597080505e5b05c
Adam Clayman <adam@clayman.org>
2017-09-30 02:02:26 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,15 +4,15 @@ let key = 0;
 function init() {
   // write your code within this function declaration
   document.addEventListener('keydown', function(event) {
-    if (event.which === code[key]) {
+    if (event.which === code[key]) { // Check if event input matches code at the key index
 
-      if (key === 9) {
+      if (key === 9) { // If the event.which and code[key] successfully match 10 times, execute 'success' code
         window.alert('Congrats! You found the Konami code!');
       }
-      key++;
+      key++; // increment the key index for each successful match
     }
     else {
-      key = 0;
+      key = 0; // for any failed match, reset the global key counter to 0
     }
   });
 }
