27ffdd21ec1051c2b677b1670459efd7f6513ba2
Adam Clayman <adam@clayman.org>
2017-09-30 02:14:02 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 
 function init() {
-  document.addEventListener('keydown', onKeyDownHandler(event));
+  document.addEventListener('keydown', onKeyDownHandler(e));
 }
 
 // This is the function that would be invoked by the event listener.
