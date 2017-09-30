061c9a6708cce446b76934bf335000a968be29e0
Adam Clayman <adam@clayman.org>
2017-09-30 00:30:29 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,24 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+  document.body.addEventListener('keydown', function(e) {
+    if (e.which === '38') {
+      for (i = 1; i < code.length; i++) {
+        if (charListener(i) === code[i]) {
+          continue;
+        }
+        else return null;
+      }
+    }
+    alert('Congrats! You found the Konami code!');
+  })
+}
 
+function charListener(i) {
+  document.body.addEventListener('keydown', function(e) {
+    if (e.which === code[i]) {
+      return e.which;
+    }
+    else return null;
+  });
 }
